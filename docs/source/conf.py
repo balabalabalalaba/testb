21605fa79271f661918a0f13c2d2b8c8c0155e10
balabalabalalaba <541361425@qq.com>
2021-08-01 15:38:55 +0800

    first commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
